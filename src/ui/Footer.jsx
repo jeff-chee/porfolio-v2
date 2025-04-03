3da5cda6e0e529fe797e72cb577b9798b1633016
Jeff Chee <jeffchee95@hotmail.com>
2025-04-03 23:49:46 +0800

    update footer text

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <div className="py-8 text-center text-xs font-light md:text-sm">
       Built with <span className="font-medium">React</span> and{" "}
       <span className="font-medium">Tailwind CSS</span>, deployed with{" "}
-      <span className="font-medium">Netlify</span>.
+      <span className="font-medium">Vercel</span>.
     </div>
   );
 }
